52c426ff1f58f11de2835ec4bf8fe6f1adff9185
Amar Mattaparthi <89722343+amar-m-cloud@users.noreply.github.com>
2023-03-29 13:18:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def predict():
     return render_template('weblone.html', Text = "For The Given Abalone Dimensions: ",Rings = "Number of Rings: {} ".format(output), Age = "Age : {} Years".format(output+1.5))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
